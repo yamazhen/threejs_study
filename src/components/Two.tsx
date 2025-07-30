91fc86225d60ebf047230dd83c4649d756b4847c
yamazhen <bowxyt@gmail.com>
2025-07-30 18:23:18 +0900

    update

--- src/components/Two.tsx
+++ src/components/Two.tsx
@@ -1,5 +1,16 @@
+import { Canvas } from "@react-three/fiber";
+
 function Two() {
-  return <div>Two</div>;
+  return (
+    <Canvas>
+      <ambientLight intensity={0.2} />
+      <pointLight args={[1, 1, 1]} intensity={1} />
+      <mesh>
+        <torusKnotGeometry args={[0.5, 0.2, 64]} />
+        <meshStandardMaterial color="red" />
+      </mesh>
+    </Canvas>
+  );
 }
 
 export default Two;
